00f248999e5e7d3c52b6da38c87fb5a093940591
Annika <gittestnack@hotmail.se>
2012-10-30 21:40:10 +0100

    First commit

---
+++ GitConnection/src/lesson/gitconnection/GitConnection.java
@@ -0,0 +1,13 @@
+package lesson.gitconnection;
+
+public class GitConnection {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
